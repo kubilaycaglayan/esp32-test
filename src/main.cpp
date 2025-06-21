80f60bac7ef00b8ed1f4c36b4b9e603918c33c5a
Kubilay Caglayan <kblycaglayan@gmail.com>
2025-06-21 21:23:59 +0300

    Blink LED with PS3 Controller

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,49 @@
 #include <Arduino.h>
-#include <Ps3Controller.h>
 #include "esp_bt_device.h"
+#include <Ps3Controller.h>
 
-#define LED_PIN 4
+#define LED_PIN 4  // Pin for the LED
+bool ledState = false;
+unsigned long lastBlinkTime = 0;
 
-void notify()
-{
-    Serial.print(Ps3.data.sensor.accelerometer.x);
-    Serial.print(" ");
-    Serial.print(Ps3.data.sensor.accelerometer.y);
-    Serial.print(" ");
-    Serial.print(Ps3.data.sensor.accelerometer.z);
+void notify() {
+  Serial.println("Data received!");
 
-    /* Uncomment the following if you also want
-       to display the gryoscope data: */
-    // Serial.print(" ");
-    // Serial.print(Ps3.data.sensor.gyroscope.z);
+  if (Ps3.data.button.cross) Serial.println("X button pressed");
+  if (Ps3.data.button.circle) Serial.println("O button pressed");
+  if (Ps3.data.button.triangle) Serial.println("Triangle button pressed");
+  if (Ps3.data.button.square) Serial.println("Square button pressed");
 
-
-}
-
-
-void setup()
-{
-    Serial.begin(115200);
-    Ps3.attach(notify);
-    Ps3.begin("3C:8A:1F:AF:7F:D2");
-}
-
-
-void loop()
-{
-  if (Ps3.isConnected()) {
     if (Ps3.data.button.cross) {
-      digitalWrite(LED_PIN, HIGH);
-    } else {
-      digitalWrite(LED_PIN, LOW);
-    }
-  }
+    unsigned long now = millis();
 
+    // Blink every 500ms while X is held
+    if (now - lastBlinkTime > 500) {
+      ledState = !ledState;
+      digitalWrite(LED_PIN, ledState ? HIGH : LOW);
+      lastBlinkTime = now;
+    }
+  } else {
+    digitalWrite(LED_PIN, LOW); // Turn off when X is released
+    ledState = false;
+  }
 }
+
+void onConnect() {
+  Serial.println("🎮 PS3 controller connected!");
+}
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(LED_PIN, OUTPUT);
+  Ps3.attach(notify);
+  Ps3.attachOnConnect(onConnect);
+  Ps3.begin("3C:8A:1F:AF:7F:D2");  // Replace with your MAC
+}
+
+void loop() {
+  // Nothing here; all handled by events
+}
+
+
+
